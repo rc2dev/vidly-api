f0aedb0f2505c99ff060b50ae1e608a855d4e5e2
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-03-18 14:29:22 -0300

    Add role-based authorization

--- routes/genres.js
+++ routes/genres.js
@@ -1,4 +1,5 @@
 const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
 const { Genre, validate } = require('../models/genre');
 const express = require('express');
 const router = express.Router();
@@ -41,7 +42,7 @@ router.put('/:id', auth, async (req, res) => {
   res.send(result);
 });
 
-router.delete('/:id', auth, async (req, res) => {
+router.delete('/:id', [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
   if (!genre)
     return res.status(404).send('The genre with the given ID was not found');
