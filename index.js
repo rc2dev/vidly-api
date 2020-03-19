0aeb42594638767a601df21cd9f8febc3e507ba0
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-03-19 18:53:38 -0300

    Add error logging

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('express-async-errors');
+const winston = require('winston');
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
@@ -12,6 +13,8 @@ const users = require('./routes/users');
 const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
+
+winston.add(new winston.transports.File({ filename: 'logfile.log' }));
 
 if (!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined.');
