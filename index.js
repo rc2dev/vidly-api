1d80b982c92a17e58775e3b94ad3a0838434b849
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-03-19 19:04:19 -0300

    Add MongoDB transport for logging

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('express-async-errors');
 const winston = require('winston');
+require('winston-mongodb');
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
@@ -15,6 +16,12 @@ const express = require('express');
 const app = express();
 
 winston.add(new winston.transports.File({ filename: 'logfile.log' }));
+winston.add(
+  new winston.transports.MongoDB({
+    db: 'mongodb://localhost/vidly',
+    metaKey: 'meta'
+  })
+);
 
 if (!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined.');
