76f573eedc9728b1d14d337021374301a1177e42
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-03-20 14:44:50 -0300

    Extract logging logic

--- index.js
+++ index.js
@@ -1,41 +1,13 @@
-require('express-async-errors');
-const winston = require('winston');
-require('winston-mongodb');
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const app = express();
 
+// Logging first, just in case we get an error when loading other modules.
+require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
-
-winston.exceptions.handle(
-  new winston.transports.File({ filename: 'exceptions.log' })
-);
-
-process.on('unhandledRejection', ex => {
-  throw ex;
-});
-
-winston.add(
-  new winston.transports.File({
-    filename: 'logfile.log',
-    format: winston.format.combine(
-      winston.format.timestamp({
-        format: 'YYYY-MM-DD hh:mm:ss A ZZ'
-      }),
-      winston.format.json()
-    )
-  })
-);
-
-winston.add(
-  new winston.transports.MongoDB({
-    db: 'mongodb://localhost/vidly',
-    metaKey: 'meta'
-  })
-);
 
 if (!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined.');
---
+++ startup/logging.js
@@ -0,0 +1,34 @@
+// In this module, we place everything related to handling and logging errors.
+
+const winston = require('winston');
+require('winston-mongodb');
+require('express-async-errors');
+
+module.exports = function() {
+  winston.exceptions.handle(
+    new winston.transports.File({ filename: 'exceptions.log' })
+  );
+
+  process.on('unhandledRejection', ex => {
+    throw ex;
+  });
+
+  winston.add(
+    new winston.transports.File({
+      filename: 'logfile.log',
+      format: winston.format.combine(
+        winston.format.timestamp({
+          format: 'YYYY-MM-DD hh:mm:ss A ZZ'
+        }),
+        winston.format.json()
+      )
+    })
+  );
+
+  winston.add(
+    new winston.transports.MongoDB({
+      db: 'mongodb://localhost/vidly',
+      metaKey: 'meta'
+    })
+  );
+};
