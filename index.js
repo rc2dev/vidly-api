40cb6c99e1822bdfbb63c2ac5879b6cc5a47ac5b
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-03-20 14:49:38 -0300

    Extract validation logic

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const app = express();
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
-require('./startup/config');
-require('./startup/validation');
+require('./startup/config')();
+require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => `Listening on port ${port}`);
