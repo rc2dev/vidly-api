c3f633e213fc810383915312627882e6fe33c59e
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-03-20 14:49:22 -0300

    Extract config logic

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const config = require('config');
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const app = express();
 
@@ -8,11 +5,8 @@ const app = express();
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
-
-if (!config.get('jwtPrivateKey')) {
-  console.error('FATAL ERROR: jwtPrivateKey is not defined.');
-  process.exit(1);
-}
+require('./startup/config');
+require('./startup/validation');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => `Listening on port ${port}`);
