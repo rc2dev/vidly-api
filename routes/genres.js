aa8683d149c37fcf27d13427618174bb07ebe83a
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-03-24 13:26:22 -0300

    Add integration tests

--- routes/genres.js
+++ routes/genres.js
@@ -1,3 +1,4 @@
+const validateObjectId = require('../middleware/validateObjectId');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 const { Genre, validate } = require('../models/genre');
@@ -9,7 +10,7 @@ router.get('/', async (req, res) => {
   res.send(genres);
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', validateObjectId, async (req, res) => {
   const genre = await Genre.findById(req.params.id);
   if (!genre)
     return res.status(404).send('The genre with the given ID was not found');
