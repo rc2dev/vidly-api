84514a4ea8516237d4ec894d19f3a39d561fff3f
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-03-25 18:08:10 -0300

    Extract rental look up logic to static method

--- models/rental.js
+++ models/rental.js
@@ -4,62 +4,68 @@ const mongoose = require('mongoose');
 // Em customer, podemos precisar de isGold para dar um deconto.
 // Em movie, precisaremos de dailyRentalRate para calcular a rental fee.
 // Incluindo ambos, não precisamos de uma additional query para calcular a rental fee.
-const Rental = mongoose.model(
-  'Rental',
-  new mongoose.Schema({
-    customer: {
-      type: new mongoose.Schema({
-        name: {
-          type: String,
-          required: true,
-          minlength: 3,
-          maxlength: 50
-        },
-        isGold: {
-          type: Boolean,
-          default: false
-        },
-        phone: {
-          type: String,
-          required: true,
-          minlength: 3,
-          maxlength: 50
-        }
-      }),
-      required: true
-    },
-    movie: {
-      type: new mongoose.Schema({
-        title: {
-          type: String,
-          required: true,
-          trim: true,
-          minlength: 3,
-          maxlength: 255
-        },
-        dailyRentalRate: {
-          type: Number,
-          required: true,
-          min: 0,
-          max: 255
-        }
-      }),
-      required: true
-    },
-    dateOut: {
-      type: Date,
-      required: true,
-      default: Date.now
-    },
-    dateReturned: {
-      type: Date
-    },
-    rentalFee: {
-      type: Number,
-      min: 0
-    }
-  })
-);
+const rentalSchema = new mongoose.Schema({
+  customer: {
+    type: new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50
+      },
+      isGold: {
+        type: Boolean,
+        default: false
+      },
+      phone: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50
+      }
+    }),
+    required: true
+  },
+  movie: {
+    type: new mongoose.Schema({
+      title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 255
+      },
+      dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255
+      }
+    }),
+    required: true
+  },
+  dateOut: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  dateReturned: {
+    type: Date
+  },
+  rentalFee: {
+    type: Number,
+    min: 0
+  }
+});
+
+rentalSchema.statics.lookup = function(customerId, movieId) {
+  return this.findOne({
+    'customer._id': customerId,
+    'movie._id': movieId
+  });
+};
+
+const Rental = mongoose.model('Rental', rentalSchema);
 
 // We don't define dateOut because it must be defined on server.
 // The same is true for dateReturned and rentalFee.
