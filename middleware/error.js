41fa44fca720c7510510d0b98f4b67f5ac6c0947
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-03-25 17:04:55 -0300

    Refactor returns: extract validate as middleware

--- middleware/error.js
+++ middleware/error.js
@@ -1,6 +1,6 @@
 const winston = require('winston');
 
 module.exports = function(err, req, res, next) {
-  winston.error('Could not get the genres', { meta: err });
+  winston.error('Error', err);
   res.status(500).send('Something failed.');
 };
--- routes/returns.js
+++ routes/returns.js
@@ -1,15 +1,13 @@
 const auth = require('../middleware/auth');
+const validate = require('../middleware/validate');
 const { Rental } = require('../models/rental');
 const { Movie } = require('../models/movie');
 const moment = require('moment');
+const Joi = require('joi');
 const express = require('express');
 const router = express.Router();
 
-router.post('/', auth, async (req, res) => {
-  if (!req.body.customerId)
-    return res.status(400).send('customerId not provided');
-  if (!req.body.movieId) return res.status(400).send('movieId not provided');
-
+router.post('/', [auth, validate(validateReturn)], async (req, res) => {
   const rental = await Rental.findOne({
     'customer._id': req.body.customerId,
     'movie._id': req.body.movieId
@@ -36,5 +34,14 @@ router.post('/', auth, async (req, res) => {
 
   res.status(200).send(rental);
 });
+
+function validateReturn(req) {
+  const schema = {
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required()
+  };
+
+  return Joi.validate(req, schema);
+}
 
 module.exports = router;
