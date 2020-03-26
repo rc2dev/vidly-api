1d43a03bb4fcacd98b641f2a5c79901fff6d88c8
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-03-26 13:59:02 -0300

    Move helmet to beginning of pipeline

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = express();
 
 // Logging first, just in case we get an error when loading other modules.
 require('./startup/logging')();
+require('./startup/prod')(app);
 require('./startup/cors')(app);
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
-require('./startup/prod')(app);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
